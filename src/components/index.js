274bd2e9ace52f576b157064ff22d20683f47bac
ramesh <ramesh@united-asian.com>
2019-07-16 15:35:29 +0545

    updated index js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
+import EventBus from '@/event-bus'
 import LocaleSwitcher from './LocaleSwitcher/Index'
 import LocaleSwitcherSSR from './LocaleSwitcherSSR/Index'
 
 export {
+  EventBus,
   LocaleSwitcher,
   LocaleSwitcherSSR
 }
