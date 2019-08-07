6fc260cdafbda5db140fae3a8c7161ad1fba039e
ramesh <ramesh@united-asian.com>
2019-08-07 13:36:39 +0545

    added test for link component

---
+++ tests/unit/LocaleSwitcher/Link.spec.js
@@ -0,0 +1,49 @@
+import {createLocalVue, shallowMount} from '@vue/test-utils'
+import Link from '@/components/LocaleSwitcher/Link.vue'
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+
+const routes = [
+  {
+    name: 'home_en',
+    path: '/'
+  },
+  {
+    name: 'home_fr',
+    path: '/fr'
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+describe('Index.vue', () => {
+  let wrapper
+
+  const label = 'en'
+  const locale = 'en'
+
+  beforeEach(() => {
+    wrapper = shallowMount(Link, {
+      localVue,
+      router,
+      propsData: {
+        label: label,
+        locale: locale
+      }
+    })
+  })
+
+  it('renders passed label prop', () => {
+    expect(wrapper.text()).toEqual(locale)
+  })
+
+  it("emits an event with locale argument", () => {
+    wrapper.vm.onLocaleChanged()
+
+    expect(wrapper.emitted()['localeChanged'][0]).toEqual(["en"])
+  })
+})
