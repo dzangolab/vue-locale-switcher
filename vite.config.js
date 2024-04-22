49b7a53cab388e6d0e47f792a665a2e51899fcfa
Gaurav Shakya <gaurav.shakya@dzangolab.com>
2024-04-22 14:23:38 +0545

    chore: add alias for bootstrap path

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import {fileURLToPath, URL} from 'url'
 import vue from '@vitejs/plugin-vue'
 
+const path = require("path")
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
