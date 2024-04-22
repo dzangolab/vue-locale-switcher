49f94a014eef392bdcfa33e31464a5937b69e40f
Gaurav Shakya <gaurav.shakya@dzangolab.com>
2024-04-22 14:47:33 +0545

    build: add build config in vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,23 @@ import { defineConfig } from 'vite'
 import {fileURLToPath, URL} from 'url'
 import vue from '@vitejs/plugin-vue'
 
-const path = require("path")
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/components/LocaleSwitcher/Index.vue',
+      name: 'localeSwitcher',
+      fileName: (format) => `localeSwitcher.${format}.js`
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-  },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  }
 })
