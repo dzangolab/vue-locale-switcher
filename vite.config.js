aa6ba205a685ca1b3d3e2e38232e949d655ea84a
Gaurav Shakya <gaurav.shakya@dzangolab.com>
2024-04-22 13:24:57 +0545

    refactor: initialize vite config file

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'url'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+})
